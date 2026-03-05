528be365d8def5c0b96d9dbc75e621c7b311282e
eledcs5566 <eledcs88@gmail.com>
2026-03-05 16:56:49 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-<script>
-  const espIP = "http://192.168.0.125";
-
-  function send(command) {
-    fetch(`${espIP}/api/${command}`)
-      .then(response => console.log("Sent:", command))
-      .catch(error => console.log("Error:", error));
-  }
-</script>
